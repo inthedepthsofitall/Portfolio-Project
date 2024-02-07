2e132cf1a2020e818209d9dd407dc3869dbee527
andrew <aihof757@gmail.com>
2024-02-07 17:58:57 -0500

    added home page

--- Portfolio Project/src/pages/About/About.tsx
+++ Portfolio Project/src/pages/About/About.tsx
@@ -4,7 +4,14 @@ const AboutPage: React.FC = () => {
   return (
     <div>
       <h1>About Me</h1>
-      <p>This is your about page content.</p>
+      <p>I started my journey into software development when I decided I wanted to create a business website for my real estate side hustle. I realized that I wanted to present 
+        to people that what I can provide as being their agent. I was able to link the mls/s, give investors some strategies when
+        looking to invest, I also try to clue the buyers and sellers on things to consider prior to moving forward with a life changing investment.
+        A few clients complimented me on the website and thought it was professional and unique as they did not see many agents taking this approach.
+        I started to look into what else I could do with software and that is when I saw content on trading bots which sparked my interest as I was already
+        investing/trading in the stock market.
+      </p>
+      <p></p>
     </div>
   );
 };
